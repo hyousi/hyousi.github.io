8fded5c1e0322df83f56cb1ca658872f840c951b
zedang <4649tz@gmail.com>
2021-07-24 22:35:59 +0800

    add eslint & prettier

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './styles/index.css'
-import App from './app/App'
+import './styles/index.css';
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './app/App';
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
-)
+  document.getElementById('root'),
+);
